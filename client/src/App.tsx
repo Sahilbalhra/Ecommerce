b04a1a0b20836ecddd1384b24a486c92d7c02d72
Sahilbalhra <sahilbalhra0@gmail.com>
2022-11-30 22:31:41 +0530

    Routes added

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,27 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
+import PageNotFound from "./Pages/404/PageNotFound";
+import About from "./Pages/About/About";
+import Auth from "./Pages/Auth/Auth";
+import Cart from "./Pages/Cart/Cart";
+import Contact from "./Pages/Contact/Contact";
+import Home from "./Pages/Home/Home";
+import Product from "./Pages/Product/Product";
+import Products from "./Pages/Products/Products";
 
 const App: React.FC = () => {
-  return <div>Hello</div>;
+  return (
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/auth' element={<Auth />} />
+      <Route path='/about' element={<About />} />
+      <Route path='/product' element={<Products />} />
+      <Route path='/cart' element={<Cart />} />
+      <Route path='/product/:id' element={<Product />} />
+      <Route path='/contact' element={<Contact />} />
+      <Route path='*' element={<PageNotFound />} />
+    </Routes>
+  );
 };
 
 export default App;
---
+++ client/src/Pages/Home/Home.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Home: React.FC = () => {
+  return (
+    <div>
+      <h1>Hello</h1>
+      <h2>Hello2</h2>
+      <p>Hello2</p>
+    </div>
+  );
+};
+
+export default Home;
